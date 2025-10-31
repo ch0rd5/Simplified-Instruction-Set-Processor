9b9d22241234719d95d73a7ea57878370318a45e
ch0rd5 <82339968+ch0rd5@users.noreply.github.com>
2025-10-30 20:10:10 -0700

    Update helpers.h

--- helpers.h
+++ helpers.h
@@ -6,7 +6,7 @@
 
 int getMemoryIndex(uint32_t address);
 
-// Instruction functions
+// Instruction declarations
 void executeMOV(const std::string& dest, const std::string& src, uint32_t reg[8], uint32_t memory[5]);
 void executeADD(const std::string& rd, const std::string& rs1, const std::string& rs2, uint32_t reg[8]);
 void executeSUB(const std::string& rd, const std::string& rs1, const std::string& rs2, uint32_t reg[8]);
@@ -21,3 +21,4 @@ void executeBNE(const std::string& label, int Z);
 void executeBAL(const std::string& label);
 
 #endif
+
